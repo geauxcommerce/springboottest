f868af95738c2536634db0f74116c58e741cf95d
Michael von Bodungen <vb@vbmacbookpro.local>
2015-05-17 23:38:45 -0300

    Configured HTTP and HTTPS (SSL) to work with embedded Tomcat.

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,14 +1,18 @@
 package hello;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-
+	
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -18,4 +22,33 @@ public class SampleController {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
     }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+      TomcatEmbeddedServletContainerFactory tomcat = new      TomcatEmbeddedServletContainerFactory() {
+          
+    	  /* @Override
+          protected void postProcessContext(Context context) {
+            SecurityConstraint securityConstraint = new SecurityConstraint();
+            securityConstraint.setUserConstraint("CONFIDENTIAL");
+            SecurityCollection collection = new SecurityCollection();
+            collection.addPattern("/*");
+            securityConstraint.addCollection(collection);
+            context.addConstraint(securityConstraint);
+          } */
+        };
+
+      tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+      return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+      Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+      connector.setScheme("http");
+      connector.setPort(8080);
+      connector.setSecure(false);
+      // connector.setRedirectPort(8443);
+
+      return connector;
+    }    
 }

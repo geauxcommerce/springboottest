3138ff1bf912fb7a4e5ce238ff6bcb76f39eca90
Michael von Bodungen <vb@vbmacbookpro.local>
2015-05-18 00:03:45 -0300

    Fixed port issue with JUnit test.

--- src/test/java/hello/SampleControllerTest.java
+++ src/test/java/hello/SampleControllerTest.java
@@ -4,7 +4,9 @@ import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,16 +21,20 @@ import static org.hamcrest.Matchers.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleController.class)
 @WebAppConfiguration
-@IntegrationTest("server.port:0")
+//@IntegrationTest("server.port:0")
+@IntegrationTest
 public class SampleControllerTest {
 	
-	@Value("${local.server.port}")
-	int port;
+	@Autowired
+	EmbeddedWebApplicationContext server;
+	
+	// @Value("${local.server.port}")
+	// int port;
 	
 	@Before
 	public void setUp() {
 		
-		RestAssured.port = port;
+		// RestAssured.port = port;
 		
 	}
 	

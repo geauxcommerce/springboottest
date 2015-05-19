65ccc71dc61d8a5fdfc8ba77b80c7ca56b56129e
Michael von Bodungen <vb@vbmacbookpro.local>
2015-05-18 23:57:25 -0300

    Updated SSL config for Tomcat and fixed JUnit tests to use SSL.

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,18 +1,28 @@
 package hello;
 
+import java.io.FileNotFoundException;
+
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 	
+	@Value("${keystore.file}") private String keystoreFile;
+	@Value("${keystore.pass}") private String keystorePass;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -22,33 +32,33 @@ public class SampleController {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
     }
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-      TomcatEmbeddedServletContainerFactory tomcat = new      TomcatEmbeddedServletContainerFactory() {
-          
-    	  /* @Override
-          protected void postProcessContext(Context context) {
-            SecurityConstraint securityConstraint = new SecurityConstraint();
-            securityConstraint.setUserConstraint("CONFIDENTIAL");
-            SecurityCollection collection = new SecurityCollection();
-            collection.addPattern("/*");
-            securityConstraint.addCollection(collection);
-            context.addConstraint(securityConstraint);
-          } */
-        };
-
-      tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-      return tomcat;
-    }
-
-    private Connector initiateHttpConnector() {
-      Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-      connector.setScheme("http");
-      connector.setPort(8080);
-      connector.setSecure(false);
-      // connector.setRedirectPort(8443);
-
-      return connector;
-    }    
+    
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer()  throws FileNotFoundException {
+		final String absoluteKeystoreFile = ResourceUtils.getFile(keystoreFile).getAbsolutePath();
+		
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(	ConfigurableEmbeddedServletContainer factory) {
+				if (factory instanceof TomcatEmbeddedServletContainerFactory) {
+					TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) factory;
+					containerFactory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+							@Override
+							public void customize(Connector connector) {
+								connector.setPort(8443);
+								connector.setSecure(true);
+								connector.setScheme("https");
+								Http11NioProtocol proto = (Http11NioProtocol) connector.getProtocolHandler();
+					            proto.setSSLEnabled(true);
+					            proto.setKeystoreFile(absoluteKeystoreFile);
+					            proto.setKeystorePass(keystorePass);
+					            proto.setKeystoreType("PKCS12");
+					            proto.setKeyAlias("tomcat");
+							}
+						});
+				}
+			}
+		};
+	}
+        
 }
--- src/test/java/hello/SampleControllerTest.java
+++ src/test/java/hello/SampleControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -21,7 +23,7 @@ import static org.hamcrest.Matchers.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleController.class)
 @WebAppConfiguration
-//@IntegrationTest("server.port:0")
+// @IntegrationTest("server.port:0")
 @IntegrationTest
 public class SampleControllerTest {
 	
@@ -34,14 +36,16 @@ public class SampleControllerTest {
 	@Before
 	public void setUp() {
 		
-		// RestAssured.port = port;
+//		 RestAssured.port = port;
+		RestAssured.keystore("keystore.p12", "localhost");
+		RestAssured.useRelaxedHTTPSValidation();
 		
 	}
 	
 	@Test
 	public void canFetchHomePage() {
 		when().
-				get("/").
+				get("https://localhost:8443").
 		then().
 				statusCode(HttpStatus.SC_OK).
 				assertThat().body(containsString("Hello World!"));

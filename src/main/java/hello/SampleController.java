d43fa2be00ba76323132a0972f1d5d51eedc96cf
Michael von Bodungen <vb@vbmacbookpro.local>
2016-05-02 22:57:23 -0300

    Made changes to output JSON

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -11,8 +11,31 @@ public class SampleController {
 	
     @RequestMapping("/")
     @ResponseBody
+    @CrossOrigin
     String home() {
-        return "Hello World!";
+		System.out.println("Inside the home method");
+        // return "{ \"title\": \"Hello World!\"}";
+		return "{"
+			+ "\"data\": ["
+		    +	"{\"id\": 21, \"name\": \"Sam\"},"
+		    +	"{\"id\": 22, \"name\": \"Izzy\"},"
+		    +	"{\"id\": 23, \"name\": \"Nathan\"},"
+		    +	"{\"id\": 24, \"name\": \"Kian\"},"
+		    +	"{\"id\": 25, \"name\": \"Tea\"},"
+		    +	"{\"id\": 26, \"name\": \"Sam\"},"
+		    +	"{\"id\": 27, \"name\": \"Tyler\"},"
+		    +	"{\"id\": 28, \"name\": \"Mark\"},"
+		    +	"{\"id\": 29, \"name\": \"Jack\"},"
+		    +	"{\"id\": 30, \"name\": \"Nicholas\"}"
+			+ "]"
+		+ "}";
+    }
+    
+    @RequestMapping("/loaderio-c0534af88b9d608b60ec5cb382de7c80/")
+    @ResponseBody
+    @CrossOrigin
+    String verify() {
+    	return "loaderio-c0534af88b9d608b60ec5cb382de7c80";
     }
 
     public static void main(String[] args) throws Exception {
